026e2c79309de7060a76299e91341045d9394d2b
Yannic Alber <yannicalber@gmail.com>
2017-06-23 00:01:04 +0200

    Selecting and order data in preProsessing.py

--- Src/preProcessing.py
+++ Src/preProcessing.py
@@ -45,57 +45,52 @@ def addInfo(dataframe, orderedArray, finalWidth, columns2Add):
     dax_newArray[i+1] = np.concatenate((orderedArray[i+1], tmp_array_p1), axis=0)
   return dax_newArray
 
-#df_dax = pd.read_csv('../Data/dax_19700105_20170428.csv', decimal=',' ,sep=';', header=0)
-df_dow = pd.read_csv('../Data/dj-IND_110101_170619.csv', decimal='.' ,sep=',', header=0)
-#df_dax = df_dax[df_dax.High != 'null']
-#df_dax = df_dax.reset_index(drop=True)
 
-#df_nikkei = pd.read_csv('../Data/Nikkei_02051985_02052017.csv', decimal='.' ,sep=',', header=0)
-#df_dowJones = pd.read_csv('../Data/DowJones_02051985_02052017.csv', decimal='.' ,sep=',', header=0)
+#df_dow = pd.read_csv('../Data/dj-IND_110101_170619.csv', decimal='.' ,sep=',', header=0)
 
-#Fuer High low data
-for i in range(len(df_dow)):
-  tmpString = list(str(df_dow.loc[i,'<DATE>']))
-  #print tmpString
-  df_dow.loc[i,'<DATE>'] = ''.join(tmpString[0:4])+'.'+''.join(tmpString[4:6])+'.' +''.join(tmpString[6:8])
+#for i in range(len(df_dow)):
+  #tmpString = list(str(df_dow.loc[i,'<DATE>']))
+  #df_dow.loc[i,'<DATE>'] = ''.join(tmpString[0:4])+'.'+''.join(tmpString[4:6])+'.' +''.join(tmpString[6:8])
   
- 
-df_dow.drop(['<TICKER>','<PER>'], axis=1,inplace=True)
-#df_nikkei.drop('Adj Close', axis=1,inplace=True)
-#df_dowJones.drop('Adj Close', axis=1,inplace=True)
+#df_dow.drop(['<TICKER>','<PER>'], axis=1,inplace=True)
+#df_dow = convertDate(df_dow, '<DATE>', '%Y.%m.%d', '2010.01.01')
+#df_dow['<TIME>'] = df_dow['<TIME>'].multiply( 1./10000. )
 
-df_dow = convertDate(df_dow, '<DATE>', '%Y.%m.%d', '2010.01.01')
-#df_dow = convertTime(df_dow, '<TIME>')
-df_dow['<TIME>'] = df_dow['<TIME>'].multiply( 1./10000. )
 
-print df_dow
+#df_dow.to_csv('../Data/dj-preStage.csv',index=False)
 
-j =0
-dayLength = []
-for i in range(len(df_dow)):
-  if df_dow.loc[i,'<TIME>'] == 0.:
-    dayLength.append( i- j )
-    j = i
 
-sieben = 0
-acht = 0
-neun = 0
-zen = 0
-elf = 0 
-zw = 0 
-dreizen = 0
-for i in range(len(dayLength)):
-  if dayLength[i] == 7: sieben+=1
-  if dayLength[i] == 8: acht+=1
-  if dayLength[i] == 9: neun+=1
-  if dayLength[i] == 10: zen+=1
-  print i
-  if dayLength[i] == 11: elf+=1
-  if dayLength[i] == 12: zw+=1
-  if dayLength[i] == 13: dreizen+=1
+df_dow = pd.read_csv('../Data/dj-preStage.csv', decimal='.' ,sep=',', header=0)
+
+#j =0
+#dayLength = []
+#for i in range(len(df_dow)):
+  #if df_dow.loc[i,'<TIME>'] == 0.:
+    #dayLength.append( i- j )
+    #j = i
+
+#sieben = 0
+#acht = 0
+#neun = 0
+#zen = 0
+#elf = 0 
+#zw = 0 
+#dreizen = 0
+#for i in range(len(dayLength)):
+  #if dayLength[i] == 7: sieben+=1
+  #if dayLength[i] == 8: acht+=1
+  #if dayLength[i] == 9: neun+=1
+  #if dayLength[i] == 10: zen+=1
+  #print i
+  #if dayLength[i] == 11: elf+=1
+  #if dayLength[i] == 12: zw+=1
+  #if dayLength[i] == 13: dreizen+=1
   
-print sieben ,acht ,neun ,zen ,elf ,zw ,dreizen 
-  
+#print sieben ,acht ,neun ,zen ,elf ,zw ,dreizen 
+
+hourList = [17.0, 18.0, 19.0, 20.0, 21.0, 22.0, 23.0, 0.0, 1.0]
+df_dow = df_dow[df_dow['<TIME>'].isin(hourList)]
+print df_dow[['<TIME>','<OPEN>','<CLOSE>']]
 
 #df_nikkei = convertDate(df_nikkei, 'Date', '%Y-%m-%d', '1985-01-01')
 #df_dowJones = convertDate(df_dowJones, 'Date', '%Y-%m-%d', '1985-01-01')
@@ -103,17 +98,19 @@ print sieben ,acht ,neun ,zen ,elf ,zw ,dreizen
 #df_combi = pd.merge(left=df_dax, right=df_nikkei, on='days')
 #df_combi = pd.merge(left=df_combi, right=df_dowJones, on='days')
 
-df_dow = df_dow.loc[:,['days','time','Open','High','Low','Close','Volume']]
+df_dow = df_dow.loc[:,['days','<TIME>','<OPEN>','<CLOSE>']]
 
+print df_dow
+df_dow = df_dow.reset_index(drop=True)
 print df_dow
 
 temp_tmp = []
-for i in range(0,len(df_dow)):
-  if int(df_dow.loc[i,'time']) == 10:
-    temp_tmp.append([df_dow.loc[i,'days'],9,df_dow.loc[i,'Open']])
-    temp_tmp.append([df_dow.loc[i,'days'],df_dow.loc[i,'time'],df_dow.loc[i,'Close']])
+for i in range(1,len(df_dow)):
+  if int(df_dow.loc[i,'<TIME>']) == 17.0 or df_dow.loc[i,'<TIME>'] == 18.0:
+    temp_tmp.append([df_dow.loc[i,'days'],df_dow.loc[i,'<TIME>']-1.,df_dow.loc[i,'<OPEN>']])
+    temp_tmp.append([df_dow.loc[i,'days'],df_dow.loc[i,'<TIME>']   ,df_dow.loc[i,'<CLOSE>']])
   else:
-    temp_tmp.append([df_dow.loc[i,'days'],df_dow.loc[i,'time'],df_dow.loc[i,'Close']])
+    temp_tmp.append([df_dow.loc[i,'days'],df_dow.loc[i,'<TIME>'],df_dow.loc[i,'<CLOSE>']])
     
 df_combi = pd.DataFrame(data=np.array(temp_tmp))
 print df_combi
